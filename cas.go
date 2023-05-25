2052ff7a663da8aa0cfddaf2c6b42e1d2a2959e8
momai <mamai2010@gmail.com>
2023-05-25 21:34:54 +0300

    update io

--- cas.go
+++ cas.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "io/ioutil"
-        "net/http"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 type CasResponse struct {
-        Ok     bool `json:"ok"`
-        Result struct {
-                Status string `json:"status"`
-        } `json:"result"`
+	Ok     bool `json:"ok"`
+	Result struct {
+		Status string `json:"status"`
+	} `json:"result"`
 }
 
 func checkUserCas(userID int64) (bool, string, error) {
-        resp, err := http.Get(fmt.Sprintf("https://api.cas.chat/check?user_id=%d", userID))
-        if err != nil {
-                return false, "", err
-        }
-        defer resp.Body.Close()
+	resp, err := http.Get(fmt.Sprintf("https://api.cas.chat/check?user_id=%d", userID))
+	if err != nil {
+		return false, "", err
+	}
+	defer resp.Body.Close()
 
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                return false, "", err
-        }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, "", err
+	}
 
-        var casResponse CasResponse
-        err = json.Unmarshal(body, &casResponse)
-        if err != nil {
-                return false, "", err
-        }
+	var casResponse CasResponse
+	err = json.Unmarshal(body, &casResponse)
+	if err != nil {
+		return false, "", err
+	}
 
-        return casResponse.Ok && casResponse.Result.Status == "ok", casResponse.Result.Status, nil
+	return casResponse.Ok && casResponse.Result.Status == "ok", casResponse.Result.Status, nil
 }
